9558c2b72b949423783958156a5765a4a078f3a4
Jake Bowkett <jake.bowkett01@gmail.com>
2021-11-05 05:46:44 +1100

    Condense type alias.

--- src/ui/type.tsx
+++ src/ui/type.tsx
@@ -6,8 +6,8 @@ Definitions for types that are not specific to one component.
 type TOnSelect    = (id: string | null, suppressHistory?: boolean) => void;
 type TSetOffPc    = (n: number)  => void;
 type TSetOffEnd   = (b: boolean) => void;
-type TSetFileId   = (id: string | undefined) => void;
-type TSetFileData = (id: string | undefined) => void;
+type TSetFileId   = (id?: string) => void;
+type TSetFileData = (id?: string) => void;
 
 export {
     TOnSelect,
