22153bf30ec3e91e10334e8f6db2d6c39326210f
Jake Bowkett <jake.bowkett01@gmail.com>
2021-11-04 20:30:53 +1100

    Use CONTENT_MAPPING to request files, extract setup/cleanup to separate function, use type aliases.

--- src/ui/App/index.tsx
+++ src/ui/App/index.tsx
@@ -1,32 +1,33 @@
 
+import "./style.css";
+import CONTENT_MAPPING from "../../content/import";
 import { useEffect, useState } from "react";
 import Sidebar from "../Sidebar";
 import Viewer from "../Viewer";
 import Range from "../Range";
 import Map from "../Map";
 import { fileList } from "../data";
-import { q } from "../tools";
-import "./style.css";
+import { q } from "../tool";
+import {
+    TOnSelect,
+    TSetOffPc,
+    TSetOffEnd,
+    TSetFileId,
+    TSetFileData,
+} from "../type";
 
 function App() {
+
     const heading = "Access Terminal Mu";
     const [ fileId, setFileId ] = useState<string | undefined>();
     const [ fileData, setFileData ] = useState<string | undefined>();
     const [ offPc, setOffPc ] = useState(0);
     const [ offEnd, setOffEnd ] = useState(false);
     const select = makeSelect(setFileId, setFileData, setOffPc, setOffEnd);
-    useEffect(() => {
-        function popState(e: PopStateEvent) { select(e.state.id, true); }
-        function scroll() { setScroll(setOffPc, setOffEnd); }
-        window.addEventListener("popstate", popState);
-        window.addEventListener("resize", scroll);
-        window.addEventListener("scroll", scroll);
-        return () => {
-            window.removeEventListener("popstate", popState);
-            window.removeEventListener("resize", scroll);
-            window.removeEventListener("scroll", scroll);
-        };
-    });
+
+    // Supplying useEffect with an empty array ensures it only runs once.
+    useEffect(setupAndCleanup(setOffPc, setOffEnd, select), []);
+
     return (
         <div id="app">
             <Sidebar
@@ -54,12 +55,57 @@ function App() {
     );
 }
 
+/* 
+    The return value of setupAndCleanup is supplied as an
+    argument to useEffect in the App component.
+
+    We return an anonymous arrow function because useEffect
+    is meant to have a new function each time it's called.
+    This way we guarantee any values we may decide to pass
+    in the future don't get stale.
+*/
+function setupAndCleanup(
+    setOffPc:  TSetOffPc,
+    setOffEnd: TSetOffEnd,
+    select:    TOnSelect,
+): () => void {
+
+    // Setup.
+    return () => {
+        
+        // Setup event listeners.
+        function popState(e: PopStateEvent) { select(e.state.id, true); }
+        function scroll() { setScroll(setOffPc, setOffEnd); }
+        window.addEventListener("popstate", popState);
+        window.addEventListener("resize", scroll);
+        window.addEventListener("scroll", scroll);
+
+        // Add approriate history object and load correct view.
+        const path = window.location.pathname.slice(PATH_PREFIX.length);
+        let id = null;
+        if (path.length > 1) {
+            id = path.slice(1);
+            select(id, true);
+        }
+        window.history.replaceState({id: id}, "", window.location.pathname);
+
+        // Cleanup.
+        return () => {
+
+            // Cleanup event listeners.
+            window.removeEventListener("popstate", popState);
+            window.removeEventListener("resize", scroll);
+            window.removeEventListener("scroll", scroll);
+        };
+    };
+}
+
 function makeSelect(
-    setFileId:   (id: string | undefined) => void,
-    setFileData: (id: string | undefined) => void,
-    setOffPc:    (n: number)  => void,
-    setOffEnd:   (b: boolean) => void,
-) {
+    setFileId:   TSetFileId,
+    setFileData: TSetFileData,
+    setOffPc:    TSetOffPc,
+    setOffEnd:   TSetOffEnd,
+): TOnSelect {
     return (id: string | null, suppressHistory?: boolean) => {
         if (!id) {
             if (!suppressHistory) {
@@ -74,7 +120,7 @@ function makeSelect(
         }
         setFileId(id);
         setFileData(`<div class="loading">Loading...</div>`);
-        fetch(`${PATH_PREFIX}/content/${id}.html`)
+        fetch(`${PATH_PREFIX}${CONTENT_MAPPING[id]}`)
             .then((res) => {
                 return res.text();
             })
@@ -89,8 +135,8 @@ function makeSelect(
 }
 
 function setScroll(
-    setOffPc:  (n: number) => void,
-    setOffEnd: (b: boolean) => void,
+    setOffPc:  TSetOffPc,
+    setOffEnd: TSetOffEnd,
 ) {
     const track = q("#range .track") as Element;
     const drag = q("#range .dragger") as Element;
